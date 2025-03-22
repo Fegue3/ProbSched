8def60340a6c4260521a8e48a8a5b960ed60fcbe
JadfPT <jadf.3407@gmail.com>
2025-03-22 20:17:19 +0000

    Main Updated

--- main.c
+++ main.c
@@ -3,18 +3,22 @@
 #include <time.h>
 #include "process.h"
 
-int main() {
-    srand(time(NULL));  // Inicializa a seed para aleatoriedade
+int main()
+{
+    srand(time(NULL)); // Inicializa a seed para aleatoriedade
 
     int count = 5;
+    double lambda_arrival = 3.0; // controla frequência de chegada (Poisson)
+    double lambda_burst = 0.5;   // controla variação de burst time (Exponencial)
+    int max_priority = 5;
 
     printf("=== Processos Estáticos ===\n");
-    Process* static_list = generate_static_processes(count);
+    Process *static_list = generate_static_processes(count);
     print_processes(static_list, count);
     free_processes(static_list);
 
     printf("\n=== Processos Aleatórios ===\n");
-    Process* random_list = generate_random_processes(count, 5, 10, 5);
+    Process* random_list = generate_random_processes(count, lambda_arrival, lambda_burst, max_priority);
     print_processes(random_list, count);
     free_processes(random_list);
 
